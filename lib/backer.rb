f6204c616cd79b672ddd6b2367fedf10433d1c92
maryfarner18 <maryfarner@gmail.com>
2019-09-19 10:28:28 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,23 @@
+
+class Backer
+    attr_reader :name
+    attr_accessor :backed_projects
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+        @@all << self
+    end
+
+    def back_project(project)
+        @backed_projects << project
+        project.add_backer(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+end # end of the Backer class
---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+
+class Project
+    attr_reader :backers, :title
+
+    def initialize(title)
+        @backers = []
+        @title = title
+    end
+
+    def add_backer(backer)
+        @backers << backer
+
+        backer.back_project(self) if !backer.backed_projects.include?(self)
+    end
+
+end # end of the Project class
