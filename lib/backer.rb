538b07c1479329d284638493d29be8238ef80510
michelle-m-lee <mlee092@gmail.com>
2019-09-21 16:44:30 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -24,3 +24,4 @@ class Backer
 
 
 end
+
--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,34 @@
-class Project
+# class Project
+
+#     attr_reader :title, :backers
+
+#     @backers = []
+
+#     def initialize(title)
+#         @title = title
+#         @backers = []
+#     end
+
+#     # Accepts a Backer as an argument and stores it in a backers array
+
+#     def add_backer(backer)
+#         @backers << backer
+#     end
+
+    
+# end
+
+class Project 
 
     attr_reader :title, :backers
-
-    @backers = []
 
     def initialize(title)
         @title = title
         @backers = []
     end
 
-    # Accepts a Backer as an argument and stores it in a backers array
-
     def add_backer(backer)
         @backers << backer
+        backer.backed_projects << self
     end
-
-    
 end
